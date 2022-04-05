96ffe9ab05d543cfb4b8cac58fab7547715a2364
aviladiogo <aviladiogo30@gmail.com>
2022-04-05 07:56:28 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('hello!')
+vezes = 0 
+function executaAcao(){
+  var resultadoD = document.getElementById('name')
+  vezes += 1
+  resultadoD.innerHTML = vezes
+}
+
+function zerar (){
+  var resultadoD = document.getElementById('name')
+  vezes = 0
+  resultadoD.innerHTML = vezes
+}
